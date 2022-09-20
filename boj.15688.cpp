7a299e8179bdc1e1c0b8a1738a433b2918df0cd6
yundaehyeok <bmong4mong0318@gmail.com>
2022-09-20 22:32:30 +0900

    20220920

---
+++ boj.15688.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#define MAX 1000000
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int n;
+    int input[MAX];
+
+    cin >> n;
+    for (int i = 0 ; i < n ; i++)
+        cin >> input[i];
+    sort(input, input + n);
+    for (int i = 0 ; i < n ; i++)
+        cout << input[i] << '\n';
+    return (0);
+}
--- boj_form.cpp
+++ boj_form.cpp
@@ -4,10 +4,9 @@ using namespace std;
 
 int main()
 {
-	ios_base::sync_with_stdio(false); 
-	cin.tie(NULL);
-	cout.tie(NULL);
-
+	// ios_base::sync_with_stdio(false); 
+	// cin.tie(NULL);
+	// cout.tie(NULL);
 }
 
 
