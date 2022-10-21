72c54379ffd1f3eb5ae133aa804f1714b4b6e59e
yundaehyeok <bmong4mong0318@gmail.com>
2022-10-21 11:42:03 +0900

    20221021

---
+++ boj_6198.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <list>
+#include <map>
+
+using namespace std;
+
+#define ffor(i, x) for (int(i) = 0; (i) < (x); (i)++)	// ⭐️ 0부터 x까지 for-loop
+#define fffor(i, x) for (int(i) = 1; (i) <= (x); ++(i)) // ⭐️ 1부터 x까지 for-loop
+#define cc(x) cin >> (x);								// ⭐️ stdin 인풋 하나 받기
+#define ccc(x) int(x);  cin >> (x);					  // ⭐️ int 변수 만들고 그걸로 stdin 인풋 하나 받기
+#define coo(x) cout << x << '\n'; // ⭐️ 변수 출력하고 줄바꿈
+#define cob(x) cout << x << ' ';  // ⭐️ 변수 출력하고 스페이스바
+#define ii pair<int, int>		  // ⭐️ 보통 vector랑 많이 쓴다
+#define ll long long			  // ⭐️ 매번 long long 치고 있으면 현기증이 옴
+
+int dx[] = {1, 0, -1, 0}; // 동 남 서 북 // ⭐️ DFS, BFS 문제 풀 때 꼭 씀
+int dy[] = {0, 1, 0, -1}; // ⭐️ 이렇게 안하고 2x4 배열로 만들어 쓰는 사람들도 있는 것 같다
+int INF = 1e9 + 7;		  // ⭐️ 의외로 자주 쓰는 수. 자주 쓰고 말고는 문제를 풀면서 생각해보면 될 문제
+
+int main()
+{
+	// ios_base::sync_with_stdio(false);
+	// cin.tie(NULL);
+	// cout.tie(NULL);
+	ccc(N)
+	stack<ii> s;
+	stack<ii> t;
+	vector<ii> v;
+	int sum = 0;
+	ffor(i, N)
+	{
+		ii x;
+		cin >> x.first;
+		x.second = i + 1;
+		v.push_back(x);
+	}
+	ffor(i, N)
+	{
+		if (s.empty())
+			s.push(v[N - i - 1]);
+		else if (v[N - i - 1].first > s.top().first)
+		{
+			while (v[N - i - 1].first > s.top().first)
+			{
+				t.push(s.top());
+				s.pop();
+				sum++;
+				if (s.empty())
+					break;
+			}
+			int tmp = v[N - i - 1].second - t.top().second;
+			if (!s.empty())
+			{
+				while (v[N - i - 1].first < s.top().first &&
+					   t.top().second < s.top().second)
+				{
+					if (tmp != 1)
+					{
+						t.pop();
+						sum++;
+					}
+					if (t.empty())
+						break;
+				}
+			}
+			s.push(v[N - i - 1]);
+		}
+		else if (v[N - i - 1].first < s.top().first)
+			s.push(v[N - i - 1]);
+	}
+	cout << sum;
+	return (0);
+}
