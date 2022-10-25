ac30be06c55a34bec5ca766a4fcba4d67d31b749
YunDaeHyeok <bmong4mong0318@gmail.com>
2022-10-25 13:08:17 +0900

    first java

---
+++ java/boj_10808.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        sc.close();
+        int alpha[] = new int[26];
+
+        for (int i = 0; i < s.length(); i++)
+            alpha[s.charAt(i)-97]++;
+        for (int i : alpha)
+            System.out.print(i+ " ");
+    }
+}
