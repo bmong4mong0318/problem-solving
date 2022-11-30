ab153691a9c879bad2c5b96f2edef4eca327a53c
yundaehyeok <bmong4mong0318@gmail.com>
2022-11-30 11:22:40 +0900

    20221130

---
+++ cpp/boj_11724.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <list>
+#include <map>
+
+using namespace std;
+
+#define ffor(i, x) for (int (i) = 0; (i) < (x) ; (i)++) // ⭐️ 0부터 x까지 for-loop
+#define fffor(i, x) for (int (i) = 1; (i) <= (x) ; ++(i)) // ⭐️ 1부터 x까지 for-loop
+#define cc(x) cin >> (x); // ⭐️ stdin 인풋 하나 받기
+#define ccc(x) int (x); cin >> (x); // ⭐️ int 변수 만들고 그걸로 stdin 인풋 하나 받기
+#define coo(x) cout << x << '\n'; // ⭐️ 변수 출력하고 줄바꿈
+#define cob(x) cout << x << ' '; // ⭐️ 변수 출력하고 스페이스바
+#define ii pair<int, int> // ⭐️ 보통 vector랑 많이 쓴다
+#define ll long long // ⭐️ 매번 long long 치고 있으면 현기증이 옴
+
+
+int dx[] = {1, 0, -1, 0}; // 동 남 서 북 // ⭐️ DFS, BFS 문제 풀 때 꼭 씀
+int dy[] = {0, 1, 0, -1}; // ⭐️ 이렇게 안하고 2x4 배열로 만들어 쓰는 사람들도 있는 것 같다
+int INF = 1e9 + 7; // ⭐️ 의외로 자주 쓰는 수. 자주 쓰고 말고는 문제를 풀면서 생각해보면 될 문제
+
+vector<int> adj[1001];
+bool visited[1001];
+
+int main()
+{
+	ios_base::sync_with_stdio(false); 
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+	while (M--){
+		int u, v;
+		cin >> u >> v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+
+	int num = 0;
+	for (int i = 1; i <= N; i++){
+		if (visited[i]) continue;
+		num++;
+		queue<int> q;
+		q.push(i);
+		visited[i] = true;
+
+		while(!q.empty()){
+			int cur = q.front();
+			q.pop();
+			for(auto next: adj[cur]){
+				if (visited[next]) continue;
+				q.push(next);
+				visited[next] = true;
+			}
+		}
+	}
+	cout << num;
+	return (0);
+}
