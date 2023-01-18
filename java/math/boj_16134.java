fc24537005a5e99ac67be2a09b32c836f8c66111
YunDaeHyeok <bmong4mong0318@gmail.com>
2023-01-18 16:23:56 +0900

    20230118

--- java/math/boj_16134.java
+++ java/math/boj_16134.java
@@ -16,7 +16,10 @@ public class boj_16134 {
         long N = Long.parseLong(st.nextToken());
         long R = Long.parseLong(st.nextToken());
 
+        //페르마의 소정리
 //        nCr = (n-r)! / r!
+//            = n! (r!(n-r)!)^(p-2) % p;
+
 
         long up = 1;
 
@@ -30,8 +33,9 @@ public class boj_16134 {
             if (tmp - tmp2 == 0)
                 up /= down;
 
+            up %= 1_000_000_007;
         }
 
-        System.out.println(up % 1_000_000_007);
+        System.out.println(up);
     }
 }
