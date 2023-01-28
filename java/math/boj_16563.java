c140f47ac1fcde50e86cfbe11a792eac41e390ce
YunDaeHyeok <bmong4mong0318@gmail.com>
2023-01-29 08:18:07 +0900

    20230129

--- java/math/boj_16563.java
+++ java/math/boj_16563.java
@@ -1,31 +1,38 @@
 package math;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class boj_16563 {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
-        ArrayList<Integer> prime = new ArrayList<>();
         // 특정 범위 내의 소수 구하기
         boolean[] isNotPrime = new boolean[5_000_001]; // int 범위 벗어나면 못함
+        int[] minFactor = new int[5_000_0001];
         // 1은 소수가 아니다
         isNotPrime[1] = true;
-        for (int i = 2; i <= 5_000_000; i++) {
+        for(int i = 2; i <= 5_000_000; i++){
+            minFactor[i] = i;
+        }
+
+        for (int i = 2; i * i <= 5_000_000; i++) {
             // 소수이이면
             if (!isNotPrime[i])
             // 그 배수들은 모두 제외
             {
-                prime.add(i);
-                for (int j = i * 2; j <= 5_000_000; j += i) {
+                for (int j = i * i; j <= 5_000_000; j += i) {
+                    if (!isNotPrime[j])
+                        minFactor[j] = i;
                     isNotPrime[j] = true;
-                    //
                 }
             }
         }
@@ -36,26 +43,32 @@ public class boj_16563 {
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             Integer num = Integer.valueOf(st.nextToken());
-            printPrime(num, prime);
+            while (num > 1){
+                bw.write(minFactor[num] + " ");
+                num /= minFactor[num];
+            }
+            bw.write("\n");
         }
+        bw.flush();
+        bw.close();
     }
 
-    private static void printPrime(Integer num, ArrayList<Integer> prime) {
-        for (Integer primeNumber : prime) {
-            num = divideByPrime(num, primeNumber);
-            if (num == 1) break;
-        }
-        System.out.println();
-    }
-
-    private static Integer divideByPrime(Integer num, int i) {
-        if (num % i == 0) {
-            num /= i;
-            System.out.print(i + " ");
-            return divideByPrime(num, i);
-        } else {
-            return num;
-        }
-    }
+//    private static void printPrime(Integer num, ArrayList<Integer> prime) {
+//        for (Integer primeNumber : prime) {
+//            num = divideByPrime(num, primeNumber);
+//            if (num == 1) break;
+//        }
+//        System.out.println();
+//    }
+//
+//    private static Integer divideByPrime(Integer num, int i) {
+//        if (num % i == 0) {
+//            num /= i;
+//            System.out.print(i + " ");
+//            return divideByPrime(num, i);
+//        } else {
+//            return num;
+//        }
+//    }
 
 }
--- java/template.java
+++ java/template.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -10,9 +12,7 @@ import java.util.StringTokenizer;
 public class template {
 
     public static class Pair { // pair
-
         int x, y;
-
         public Pair(int x, int y) {
             this.x = x;
             this.y = y;
@@ -25,7 +25,11 @@ public class template {
     static int[] dy = {0, 1, 0, -1};
     static int[][] map; // 2차원 지도
 
+    // 빠른 입력
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    // 빠른 출력
+    // 버퍼를 잡아 놓았기 때문에 bw.flush(); bw.close(); 를 반드시 호출 해주어야 한다.
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
@@ -76,19 +80,17 @@ public class template {
 
     // 특정 범위 내의 소수 구하기 - 에라토스테네스의 체 활용
     private static ArrayList<Integer> getPrimeNumbers(int LIMIT) {
-        int sqrtN = (int)Math.sqrt(LIMIT);
         ArrayList<Integer> prime = new ArrayList<>();
-        boolean[] isNotPrime = new boolean[sqrtN + 1]; // int 범위 벗어나면 못함
+        boolean[] isNotPrime = new boolean[LIMIT + 1]; // int 범위 벗어나면 못함
         // 1은 소수가 아니다
         isNotPrime[1] = true;
-
-        for (int i = 2; i <= sqrtN; i++) {
+        for (int i = 2; i * i <= LIMIT; i++) {
             // 소수이면
             if (!isNotPrime[i])
             {
                 //소수만 모으고
                 prime.add(i);
-                for (int j = i * 2; j <= sqrtN; j += i) {
+                for (int j = i * i; j <= LIMIT; j += i) {
                     // 그 배수들은 모두 제외
                     isNotPrime[j] = true;
                 }
