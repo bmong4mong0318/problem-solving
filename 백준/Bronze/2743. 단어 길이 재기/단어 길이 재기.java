7e91f1478c32bc300559c6606e8956af0ae010ce
DaeHyeok Yun <96916551+bmong4mong0318@users.noreply.github.com>
2023-03-09 15:58:16 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static String a, b;
+
+    public static void main(String[] args) throws IOException {
+
+//        Scanner in = new Scanner(System.in);
+        st = new StringTokenizer(br.readLine());
+        a = st.nextToken();
+        
+        System.out.println(a.length());
+    }
+}
