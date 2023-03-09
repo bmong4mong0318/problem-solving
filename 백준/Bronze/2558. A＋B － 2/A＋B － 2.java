7ecc986f28e278f839d8a19fb8ad012364d28dd1
DaeHyeok Yun <96916551+bmong4mong0318@users.noreply.github.com>
2023-03-09 15:29:31 +0900

    [Bronze V] Title: A+B - 2, Time: 208 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int a, b;
+    
+    public static void main(String[] args) throws IOException {
+
+        Scanner in = new Scanner(System.in);
+        a = in.nextInt();
+        b = in.nextInt();
+
+        System.out.println(a + b);
+    }
+}
