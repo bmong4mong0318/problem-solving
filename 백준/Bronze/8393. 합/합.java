2dc871a1559843dc729e9f8fa2bbd14ff944cdd3
DaeHyeok Yun <96916551+bmong4mong0318@users.noreply.github.com>
2023-03-13 08:56:35 +0900

    [Bronze V] Title: 합, Time: 204 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int a, b;
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        a = in.nextInt();
+
+        System.out.println((a*(a+1)/2));
+    }
+}
