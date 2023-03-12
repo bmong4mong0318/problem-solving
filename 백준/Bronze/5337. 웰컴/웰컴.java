36a64173f0546075299df9b04754acb8e5bedda4
DaeHyeok Yun <96916551+bmong4mong0318@users.noreply.github.com>
2023-03-12 21:38:57 +0900

    [Bronze V] Title: 웰컴, Time: 124 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static String a, b;
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
