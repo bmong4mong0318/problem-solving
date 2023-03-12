a4916ff30438ae4e490795692c5e64f1f068aa84
DaeHyeok Yun <96916551+bmong4mong0318@users.noreply.github.com>
2023-03-12 21:45:39 +0900

    [Bronze V] Title: 콜센터, Time: 116 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static String a, b;
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("     /~\\");
+        System.out.println("    ( oo|");
+        System.out.println("    _\\=/_");
+        System.out.println("   /  _  \\");
+        System.out.println("  //|/.\\|\\\\");
+        System.out.println(" ||  \\ /  ||");
+        System.out.println("============");
+        System.out.println("|          |\n"
+            + "|          |\n"
+            + "|          |");
+    }
+}
