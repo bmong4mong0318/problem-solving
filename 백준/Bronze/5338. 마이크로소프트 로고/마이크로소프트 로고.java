f7ff1fffcda3645161162b6667484a6fca9e638c
DaeHyeok Yun <96916551+bmong4mong0318@users.noreply.github.com>
2023-03-12 21:42:49 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 116 ms, Memory: 13968 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static String a, b;
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-''-._|");
+    }
+}
