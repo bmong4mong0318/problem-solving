d2aa9703118bef9bd8d3382b831b66fd39a0d4f4
DaeHyeok Yun <96916551+bmong4mong0318@users.noreply.github.com>
2023-03-13 08:53:17 +0900

    [Bronze V] Title: 등록, Time: 116 ms, Memory: 13964 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static String a, b;
+    
+    public static void main(String[] args) {
+        System.out.println(163);
+        System.out.println("bmong4mong0318");
+    }
+}
