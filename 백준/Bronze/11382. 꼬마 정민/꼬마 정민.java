4a2d3a641e846b111cae0adbd8128130f3fa5fde
DaeHyeok Yun <96916551+bmong4mong0318@users.noreply.github.com>
2023-03-13 09:46:19 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static Long A, B, C;
+    static String str;
+
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+
+        A = in.nextLong();
+        B = in.nextLong();
+        C = in.nextLong();
+
+        System.out.println(A + B + C);
+    }
+}
