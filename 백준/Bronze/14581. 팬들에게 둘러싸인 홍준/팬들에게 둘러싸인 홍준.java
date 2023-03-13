207804ed8973ba1cad5ed078bae9679fd776a8be
DaeHyeok Yun <96916551+bmong4mong0318@users.noreply.github.com>
2023-03-13 10:15:21 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 152 ms, Memory: 15868 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static BigInteger A, B, C;
+    static String str;
+
+    public static void main(String[] args) throws IOException {
+
+        str = br.readLine();
+
+        System.out.println(":fan::fan::fan:\n"
+            + ":fan::" + str + "::fan:\n"
+            + ":fan::fan::fan:");
+    }
+}
