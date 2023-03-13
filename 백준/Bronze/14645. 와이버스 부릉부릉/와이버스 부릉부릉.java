ce454ce1a41740a8244a654ff2e73f08b1524b58
DaeHyeok Yun <96916551+bmong4mong0318@users.noreply.github.com>
2023-03-13 10:17:24 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 120 ms, Memory: 13940 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static BigInteger A, B, C;
+    static String str;
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("비와이");
+    }
+}
