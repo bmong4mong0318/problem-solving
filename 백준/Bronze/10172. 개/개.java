36b73c3040e74efdfc7c05859bcba20889a2dab8
DaeHyeok Yun <96916551+bmong4mong0318@users.noreply.github.com>
2023-03-13 09:06:12 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13992 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int a, b;
+
+    public static void main(String[] args) {
+        System.out.println("|\\_/|\n"
+            + "|q p|   /}\n"
+            + "( 0 )\"\"\"\\\n"
+            + "|\"^\"`    |\n"
+            + "||_/=\\\\__|");
+    }
+}
