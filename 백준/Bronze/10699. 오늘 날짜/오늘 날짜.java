4cabe22d8ca8090bfe6fc99b8866983b8aea3a26
DaeHyeok Yun <96916551+bmong4mong0318@users.noreply.github.com>
2023-03-13 09:10:30 +0900

    [Bronze V] Title: 오늘 날짜, Time: 112 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int A, B, C;
+
+    public static void main(String[] args) {
+        System.out.println("2023-03-13");
+    }
+}
