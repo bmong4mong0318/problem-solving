dc878cece02bfe3675be7dfb030a4323c8317301
DaeHyeok Yun <96916551+bmong4mong0318@users.noreply.github.com>
2023-03-13 10:06:49 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 132 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int A, B, C;
+    static String str;
+
+    public static void main(String[] args) throws IOException {
+
+        while ((str = br.readLine()) != null){
+            System.out.println(str);
+        }
+
+    }
+}
