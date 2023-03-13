9c3cee949f974fd907e45b52718b60a4c56b3f44
DaeHyeok Yun <96916551+bmong4mong0318@users.noreply.github.com>
2023-03-13 09:43:16 +0900

    [Bronze V] Title: A+B - 8, Time: 280 ms, Memory: 19060 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static Integer A, B, C;
+    static String str;
+
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+
+        A = in.nextInt();
+
+        for (int i = 1; i <= A; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+
+            System.out.println("Case #" + i + ": "
+                + a + " + " + b + " = " +(a + b));
+        }
+    }
+}
