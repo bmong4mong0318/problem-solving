264eda936b9325532f2ef8112290ad5801121e49
DaeHyeok Yun <96916551+bmong4mong0318@users.noreply.github.com>
2023-03-13 09:05:11 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 116 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int a, b;
+
+    public static void main(String[] args) {
+        System.out.println("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n"
+            + "N2 Bomber      Heavy Fighter  Limited    21        \n"
+            + "J-Type 327     Light Combat   Unlimited  1         \n"
+            + "NX Cruiser     Medium Fighter Limited    18        \n"
+            + "N1 Starfighter Medium Fighter Unlimited  25        \n"
+            + "Royal Cruiser  Light Combat   Limited    4   ");
+    }
+}
