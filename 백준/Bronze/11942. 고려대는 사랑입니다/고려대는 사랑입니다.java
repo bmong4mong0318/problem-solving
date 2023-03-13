b91ea9cab3329a6f12038ce588c91a079686dfad
DaeHyeok Yun <96916551+bmong4mong0318@users.noreply.github.com>
2023-03-13 10:07:45 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 120 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int A, B, C;
+    static String str;
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("고려대학교");
+
+    }
+}
