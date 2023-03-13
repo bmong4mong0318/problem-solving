ad8ea2b22a3f39897151554427ce81d8f47e8c94
DaeHyeok Yun <96916551+bmong4mong0318@users.noreply.github.com>
2023-03-13 10:00:57 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int A, B, C;
+    static String str;
+
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+
+        A = in.next().charAt(0);
+
+        System.out.println(A);
+    }
+}
