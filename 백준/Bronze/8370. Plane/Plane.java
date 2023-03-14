86a84f6c742ec0350ac4eefdef978c5489f761d9
DaeHyeok Yun <96916551+bmong4mong0318@users.noreply.github.com>
2023-03-15 01:33:22 +0900

    [Bronze V] Title: Plane, Time: 208 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.java
@@ -0,0 +1,25 @@
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int N, M;
+    static String str;
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int d = in.nextInt();
+
+        System.out.println(a*b + c*d);
+    }
+}
