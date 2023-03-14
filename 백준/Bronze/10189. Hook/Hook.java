e01a3cc758fe305e2c05535f34d770cc896e7d22
DaeHyeok Yun <96916551+bmong4mong0318@users.noreply.github.com>
2023-03-15 01:58:44 +0900

    [Bronze V] Title: Hook, Time: 112 ms, Memory: 14020 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,22 @@
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int N;
+    static String str;
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("#  # #### #### #  #\n"
+            + "#### #  # #  # # #\n"
+            + "#### #  # #  # # #\n"
+            + "#  # #### #### #  #");
+
+    }
+}
