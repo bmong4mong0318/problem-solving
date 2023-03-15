5274640425842f7a15b84e0b22d45f7485073cdb
DaeHyeok Yun <96916551+bmong4mong0318@users.noreply.github.com>
2023-03-16 01:09:58 +0900

    [Bronze V] Title: 엔드게임 스포일러, Time: 124 ms, Memory: 13980 KB -BaekjoonHub

---
+++ 백준/Bronze/17295. 엔드게임 스포일러/엔드게임 스포일러.java
@@ -0,0 +1,19 @@
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int N;
+    static int a, b;
+    static String str;
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Avengers: Endgame");
+    }
+}
