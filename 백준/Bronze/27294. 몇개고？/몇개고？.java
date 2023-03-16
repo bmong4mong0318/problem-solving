81a3e5e73664d52e861674ad9f9c34ac7640615d
DaeHyeok Yun <96916551+bmong4mong0318@users.noreply.github.com>
2023-03-17 08:30:54 +0900

    [Bronze V] Title: 몇개고?, Time: 124 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/27294. 몇개고？/몇개고？.java
@@ -0,0 +1,26 @@
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static BigInteger N, M;
+    static String str;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        if (m == 1 || !(n >= 12 && n <= 16)) {
+            System.out.println(280);
+        } else if ((n >= 12 && n <= 16) && m == 0) {
+            System.out.println(320);
+        }
+
+    }
+}
