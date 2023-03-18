68d2670920555f8ae6dee741a5effbac7458a55d
YunDaeHyeok <bmong4mong0318@gmail.com>
2023-03-18 17:31:34 +0900

    20230318

--- java/Main.java
+++ java/Main.java
@@ -8,15 +8,20 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
-    static int N;
+    static BigInteger N, M;
     static String str;
 
     public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        System.out.println("#  # #### #### #  #\n"
-            + "#### #  # #  # # #\n"
-            + "#### #  # #  # # #\n"
-            + "#  # #### #### #  #");
+        if (m == 1 || !(n >= 12 && n <= 16)) {
+            System.out.println(280);
+        } else if ((n >= 12 && n <= 16) && m == 0) {
+            System.out.println(320);
+        }
+
 
     }
 }
