0c9f0616983400b7a4fd6b8aab34c448b4b9e0dc
DaeHyeok Yun <96916551+bmong4mong0318@users.noreply.github.com>
2023-03-23 21:07:47 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 212 ms, Memory: 14876 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,30 @@
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static String str;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+
+        st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+
+        for(int i = 1; i <= N; i++){
+            for (int j = 0; j <= N - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
+
