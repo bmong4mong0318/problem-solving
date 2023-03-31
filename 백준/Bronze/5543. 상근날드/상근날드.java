193aaf28347be9599f0ffa5feee7f52cc8515fc2
DaeHyeok Yun <96916551+bmong4mong0318@users.noreply.github.com>
2023-03-31 10:18:36 +0900

    [Bronze IV] Title: 상근날드, Time: 204 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb;
+    static String str;
+
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int d = in.nextInt();
+        int e = in.nextInt();
+
+        System.out.println(Math.min(a, Math.min(b, c)) + Math.min(d, e) - 50);
+    }
+}
